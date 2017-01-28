8db9872c1adb1e4db02afe4fd6b7a8f7f31ae414
Tobias <tobias.hartmann@sitewards.com>
2017-01-28 12:41:58 +0100

    static domain and post template preparation

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp  = require('gulp');
-
-var distFolder = 'web';
+var gulp = require('gulp');
 
 gulp.task('hbs', function () {
     var handlebars = require('gulp-compile-handlebars');
     var pageConfig = require('./src/config.json');
     var options = {
         ignorePartials: true,
-        batch : ['./src/partials']
+        batch: ['./src/partials'],
+        helpers: {
+          formatTags: require('./script/helper/formatTags')
+        }
     }
 
     return gulp.src('./src/**/*.html')
         .pipe(handlebars(pageConfig, options))
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest('web'));
 });
 
 gulp.task('css', function () {
@@ -20,17 +21,17 @@ gulp.task('css', function () {
     var sourcemaps = require('gulp-sourcemaps');
     var atImport = require('postcss-import');
 
-    return gulp.src('./src/**/*.css')
+    return gulp.src('./src/css/styles.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([
           atImport({
-            path: ['node_modules','src/css'],
+            path: ['./node_modules','./src/css'],
           }),
           require('precss'),
           require('autoprefixer')
         ]))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest('web/assets/css'));
 });
 
 gulp.task('default', ['hbs','css']);
